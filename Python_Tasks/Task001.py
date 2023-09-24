809e4a6228e0ae7c49ae7e2d291f1d21e3c8f146
Nofre417 <nofrevideo@gmail.com>
2023-09-24 18:27:23 +0200

    Задача 3. С помошь функции, алгаритмов рекурсии и сортировок создали программу для нахождения и сортировки числа

--- Python_Tasks/Task001.py
+++ Python_Tasks/Task001.py
@@ -1,11 +1,3 @@
-# username = input('Your name: ' )
-# if username == 'David':
-#     print('David its me')
-# elif username == 'Eva':
-#     print('Eva nice girl')
-# else:
-#     print('Hi ', username)
-
 def sum_finder(n):
     sum = 0
     for i in range(1,n + 1):
