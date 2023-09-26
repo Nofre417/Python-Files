2d5cf93d0b07be4c47a695530eff727cd350426a
Nofre417 <nofrevideo@gmail.com>
2023-09-26 21:08:09 +0200

    Added Task004. Just some template file

---
+++ Python_Tasks/Task004.py
@@ -0,0 +1,19 @@
+# a = 3
+# b = 5
+
+# def f(a, b):
+#     if b == 0:
+#         return 1
+#     return a * f(a, b - 1)
+    
+# print(f(a))
+
+# a = 4
+# b = 10
+
+# def sum(a, b):
+#     if b == 0:
+#         return a
+#     return sum(a + 1, b - 1)
+
+# print(sum(a, b))
